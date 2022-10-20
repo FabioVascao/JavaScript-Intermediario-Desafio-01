cc0dbb679f685e49fc12030e32226b7f125bef16
Fabio de Sousa Santos <109177231+FabioVascao@users.noreply.github.com>
2022-10-20 13:34:23 -0300

    sounds

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const buttonForest = document.querySelector('.Forest')
+
+const buttonPressForest = new Audio("https://drive.google.com/file/d/1CRHkV72WUMdcqec5GT_KdsqFz0z3VAOA/view")
+
+function pressButton() {
+  buttonPressForest.play();
+}
