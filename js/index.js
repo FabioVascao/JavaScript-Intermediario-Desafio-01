ec9b2f353038aac07057ac97579f543debf89618
Fabio de Sousa Santos <109177231+FabioVascao@users.noreply.github.com>
2022-10-20 14:12:55 -0300

    sons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
-const buttonForest = document.querySelector('.Forest')
+const buttonForest = document.querySelector('#forest')
+const buttonRain = document.querySelector('#rain')
+const buttonMarket = document.querySelector('#market')
+const buttonFire = document.querySelector('#fire')
 
-const buttonPressForest = new Audio("https://drive.google.com/file/d/1CRHkV72WUMdcqec5GT_KdsqFz0z3VAOA/view")
+const buttonPressForest = new Audio("https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Floresta.wav?raw=true")
+const buttonPressRain = new Audio('https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Chuva.wav?raw=true')
+const buttonPressMarket = new Audio('https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Cafeteria.wav?raw=true')
+const buttonPressFire = new Audio('https://github.com/FabioVascao/JavaScript-Intermediario-Desafio-01/blob/main/sounds/Lareira.wav?raw=true')
 
-function pressButton() {
+function pressButtonForest() {
   buttonPressForest.play();
 }
+
+function pressButtonRain(){
+  buttonPressRain.play();
+}
+
+function pressButtonMarket(){
+  buttonPressMarket.play();
+}
+
+function pressButtonFire(){
+  buttonPressFire.play();
+}
+
+buttonForest.addEventListener('click', function(){ 
+  pressButtonForest();
+})
+
+buttonRain.addEventListener('click', function(){ 
+  pressButtonRain();
+})
+
+buttonMarket.addEventListener('click', function(){ 
+  pressButtonMarket();
+})
+
+buttonFire.addEventListener('click', function(){ 
+  pressButtonFire();
+})
+
+
